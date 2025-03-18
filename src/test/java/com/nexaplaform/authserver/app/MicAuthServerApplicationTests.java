a76656e7f7a7d2be5e59dbf5f42757341615cfa8
Jesis Finol <jfinol02@gmail.com>
2025-03-18 21:16:22 +0100

    Creación basica del authserver

--- src/test/java/com/nexaplaform/authserver/app/MicAuthServerApplicationTests.java
+++ src/test/java/com/nexaplaform/authserver/app/MicAuthServerApplicationTests.java
@@ -1,15 +1,15 @@
 package com.nexaplaform.authserver.app;
 
+import com.nexaplaform.authserver.app.configuration.TestcontainersConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
-class MicAuthserverApplicationTests {
+public class MicAuthServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
